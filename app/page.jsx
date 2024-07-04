09df029173c14196ba00c460d9b456d678f42dce
Andrew Ye <andrewye16@gmail.com>
2024-07-04 16:39:47 -0400

    fix
    Signed-off-by: Andrew Ye <andrewye16@gmail.com>

--- app/page.jsx
+++ app/page.jsx
@@ -36,10 +36,10 @@ export default async function Home() {
           />
           <div className='lg:w-1/2'>
             <h2 className='text-3xl font-bold mb-2'>A little more about me</h2>
-            <p className='mb-2'>I'm a recent University of Michigan grad, where I studied Data Science. I have a background in data science & engineering, software development, and quantitative research. I have honed my skills in data engineering, statistical analysis, and project management through diverse research projects and leadership roles on campus.</p>
+            <p className='mb-2'>I&apos;m a recent University of Michigan grad, where I studied Data Science. I have a background in data science & engineering, software development, and quantitative research. I have honed my skills in data engineering, statistical analysis, and project management through diverse research projects and leadership roles on campus.</p>
             <p className="mb-2">Currently, I am a Research Assistant at the Stephen M. Ross School of Business, where I lead data analytics efforts to explore the impact that corporate mission and values has on attracting candidates.</p>
             <p className="mb-2">Core languages that I am fluent in include C++, Java, Python, SQL, and R. I am passionate about leveraging data to drive meaningful insights and business decisions, and I am eager to contribute to innovative projects and businesses to further develop my expertise in data science and engineering.</p>
-            <p className='mb-4'>I am also an avid hiker and traveler. I&apos;ve been to 30 different US states and 10 national parks - my favorite hike is Angels Landing. I also enjoy going to Michigan Football games - Go Blue!</p>
+            <p className='mb-4'>I am also an avid hiker and traveler. I&apos;ve been to 30 different US states and 10 national parks - my favorite hike is Angels Landing. I enjoy going to Michigan Football games as well - Go Blue!</p>
           </div>
         </div>
       </section>
