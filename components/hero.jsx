105bd1b82b7a75ed7bfa3bf1455ad996390fe8fc
Andrew Ye <andrewye16@gmail.com>
2024-07-29 17:26:15 -0400

    update
    Signed-off-by: Andrew Ye <andrewye16@gmail.com>

--- components/hero.jsx
+++ components/hero.jsx
@@ -12,7 +12,7 @@ export default function Hero() {
             </span>
           </h1>
           <p className="mt-2">
-          University of Michigan graduate intersted in using data to solve problems and make informed decisions. Research Assistant at the Ross School of Business.
+          University of Michigan graduate intersted in how data help us to solve problems and make informed decisions. Research Assistant at the Ross School of Business.
           </p>
           <div className="flex justify-start gap-5 pt-2 flex-wrap">
 
